51da76815189f82e5d5a74c42e8afaa142f6e87d
kadir <kadir.tepecik1903@gmail.com>
2022-07-27 12:31:24 +0300

    27/07/2022

---
+++ cypress/integration/demo/day02_02_loginTest.js
@@ -0,0 +1,44 @@
+describe("Login ",()=>{
+
+    const email = "qwert@gmail.com";
+  const psw = "qwert";
+   
+    var url="https://www.automationexercise.com/"
+    const email1 = "jeffry.muller@hotmail.com";
+  const psw1 = "123abc";
+  it.skip("Test negatif ",()=>{
+   
+    cy.visit(url);
+    cy.get('.shop-menu > .nav > :nth-child(4) > a').click();
+    cy.get('.login-form > form > [type="email"]').type(email);
+    cy.get('[type="password"]').type(psw);
+    cy.get('.login-form > form > .btn').click();
+
+    cy.get('.login-form > form > p').should("be.visible");
+    cy.get('.login-form > form > p').should("have.text","Your email or password is incorrect!");
+    
+
+
+  })
+  it("Test pozitif",()=>{
+ 
+   cy.visit(url);
+   cy.get('.shop-menu > .nav > :nth-child(4) > a').click();
+    cy.get('.login-form > form > [type="email"]').type(email1);
+    cy.get('[type="password"]').type(psw1);
+    cy.get('.login-form > form > .btn').click();
+    cy.get(':nth-child(10) > a').should("be.visible");
+   
+
+    
+
+
+  })
+
+
+
+
+
+
+
+})
